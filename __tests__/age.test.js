de320c553592a9a4158fc3d4fe95ef29feebe7a5
Elena <elenarosamail@gmail.com>
2023-08-05 16:58:22 -0700

    Written Test 02

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -7,4 +7,11 @@ describe('Age', () => {
         expect(newAge.years).toBe(47);
 
     });
-}); 
+});
+
+describe('Age.getMercuryYears()', () => {
+    test("returns age for Mercury years", () => {
+        const newAge = new Age(47)
+        expect(newAge.getMercuryYears()).toBeCloseTo(196, .1);
+    });
+});
