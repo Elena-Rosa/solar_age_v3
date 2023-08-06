a661b3cacc657c55552b8188280c894d78a6b85e
Elena <elenarosamail@gmail.com>
2023-08-06 09:15:38 -0700

    Written Test Future Years

---
+++ __tests__/futureYears.test.js
@@ -0,0 +1,12 @@
+describe('newAge.getPastYears', () => {
+    test("returns future age for Earth, Mercury, Venus, Mars and Jupiter", () => {
+
+        const newAge = new Age(47)
+        let future = newAge.getFutureYears(47, 15);
+        expect(future[0]).toBeCloseTo(32, .1);
+        expect(future[1]).toBeCloseTo(58, .1);
+        expect(future[2]).toBeCloseTo(52, .1);
+        expect(future[3]).toBeCloseTo(59, .1);
+        expect(future[4]).toBeCloseTo(69, .1);
+    });
+});
